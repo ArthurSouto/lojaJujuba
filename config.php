8157f2f77b506e328f94a81ca09d7f3f112cf12b
ArthurSouto <arthursouto2005@gmail.com>
2022-10-07 21:52:30 -0300

    finalizado o sistema de gestão da base de dados

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ define('APP_VERSION',       '1.0.0');
 define('MYSQL_SERVER',      'localhost');
 define('MYSQL_DATABASE',    'jujuba');
 define('MYSQL_USER',        'user_jujuba');
-define('MYSQL_PASS',        '');
+define('MYSQL_PASS',        'H1GidezEJIv7');
 define('MYSQL_CHARSET',     'utf8');
--- core/classes/Database.php
+++ core/classes/Database.php
@@ -2,6 +2,7 @@
 
 namespace core\classes;
 
+use Exception;
 use PDO;
 use PDOException;
 
@@ -31,11 +32,18 @@ class Database{
         // desligar a base de dados
         $this->ligacao = null;
     }
+    
     // ============================================================
     //CRUD
     // ============================================================
     public function select($sql, $parametros = null){
-        //executar função de pesquisa de SQL
+        
+        //verifica se é uma instrução SELECT
+        if(!preg_match("/^SELECT/i", $sql)){
+            throw new Exception('Base de Dados - Não é uma intrução SELECT.');
+        }
+
+        //liga
         $this->ligar();
 
         $resultados = null;
@@ -58,10 +66,145 @@ class Database{
             return false;
         }
 
-        // desligar da BD
+        // desliga da BD
         $this->desligar();
 
         //devolder resultado obtidos
         return $resultados;
+    }
+
+    // ============================================================
+    public function insert($sql, $parametros = null){
+        
+        //verifica se é uma instrução INSERT
+        if(!preg_match("/^INSERTT/i", $sql)){
+            throw new Exception('Base de Dados - Não é uma intrução INSERT.');
+        }
+
+        // liga
+        $this->ligar();
+
+        $resultados = null;
+
+        //comunicar
+        try {
+            // comunicação com a BD
+            if(!empty($parametros)){
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute($parametros);
+            } else {
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute();
+            }
+
+        } catch (PDOException $e) {
+            // caso exista erro
+            return false;
+        }
+
+        // desligar da BD
+        $this->desligar();
+    }
+
+    // ============================================================
+    public function update($sql, $parametros = null){
+        
+        //verifica se é uma instrução INSERT
+        if(!preg_match("/^UPDATE/i", $sql)){
+            throw new Exception('Base de Dados - Não é uma intrução UPDATE.');
+        }
+
+        // liga
+        $this->ligar();
+
+        $resultados = null;
+
+        //comunicar
+        try {
+            // comunicação com a BD
+            if(!empty($parametros)){
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute($parametros);
+            } else {
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute();
+            }
+
+        } catch (PDOException $e) {
+            // caso exista erro
+            return false;
+        }
+
+        // desligar da BD
+        $this->desligar();
+    }
+
+    // ============================================================
+    public function delete($sql, $parametros = null){
+        
+        //verifica se é uma instrução INSERT
+        if(!preg_match("/^DELETE/i", $sql)){
+            throw new Exception('Base de Dados - Não é uma intrução DELETE.');
+        }
+
+        // liga
+        $this->ligar();
+
+        $resultados = null;
+
+        //comunicar
+        try {
+            // comunicação com a BD
+            if(!empty($parametros)){
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute($parametros);
+            } else {
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute();
+            }
+
+        } catch (PDOException $e) {
+            // caso exista erro
+            return false;
+        }
+
+        // desligar da BD
+        $this->desligar();
+    }
+
+
+    // ============================================================
+    //GENÉRICA
+    // ============================================================
+    public function statement($sql, $parametros = null){
+        
+        //verifica se é uma instrução diferente das anteriores
+        if(preg_match("/^(SELECT|INSERT|UPDATE|DELETE)/i", $sql)){
+            throw new Exception('Base de Dados - instrução invalida.');
+        }
+
+        // liga
+        $this->ligar();
+
+        $resultados = null;
+
+        //comunicar
+        try {
+            // comunicação com a BD
+            if(!empty($parametros)){
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute($parametros);
+            } else {
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute();
+            }
+
+        } catch (PDOException $e) {
+            // caso exista erro
+            return false;
+        }
+
+        // desligar da BD
+        $this->desligar();
     }
 }
